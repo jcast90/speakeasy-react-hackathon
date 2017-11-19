e026b528e073f59fa10ece133ed1e75051100488
Jonathan Lancaster <jcast90@me.com>
2017-11-19 10:42:24 -0800

    made app more user friendly

--- src/components/Add.jsx
+++ src/components/Add.jsx
@@ -4,18 +4,52 @@ export class Add extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      active: false
+      active: false,
+      close: false,
+      hidden: ''
     };
+    this.closeIntro = this.closeIntro.bind(this);
   }
-
+  closeIntro() {
+    if (this.state.close === false) {
+      this.setState({
+        close: true,
+        hidden: 'hidden'
+      });
+    } else {
+      this.setState({
+        close: false,
+        hidden: ''
+      });
+    }
+  }
   render() {
+    var hours = new Date().getHours();
     return (
       <div className="add-wrapper">
         <div className="add-mood-wrapper">
           <header className="add-header">
-            <h1 className="add-title">
-              Today's Mood: <span className="userMood">{this.props.mood}</span>
-            </h1>
+            <div className={`first-timers ${this.state.hidden}`}>
+              <span
+                className="close-icon glyphicon glyphicon-remove"
+                onClick={this.closeIntro}
+              />
+              <h1> Welcome To Speakeasy!</h1>
+              <h3>
+                An app that will give you a spotify playlist and cocktail based
+                on your mood!
+              </h3>
+              <span>
+                To get started click on a mood and then click mix it up!
+              </span>
+            </div>
+            <h2 className="add-title">
+              {hours < 12 ? "Today's" : "Tonight's"} Mood:{' '}
+              <span className="userMood">{this.props.mood}</span>
+            </h2>
+            <button className="btn btn-mix" onClick={this.props.button}>
+              Mix It Up
+            </button>
           </header>
           <div className="mood-item">
             <div
@@ -62,13 +96,13 @@ export class Add extends Component {
             </div>
           </div>
         </div>
-
-        <div className="add-button-wrapper">
-          <button className="btn btn-mix" onClick={this.props.button}>
-            Mix It Up
-          </button>
-        </div>
       </div>
     );
   }
 }
+
+// <div className="add-button-wrapper">
+// <button className="btn btn-mix" onClick={this.props.button}>
+//   Mix It Up
+// </button>
+// </div>
